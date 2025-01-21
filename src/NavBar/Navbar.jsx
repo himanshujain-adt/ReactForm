033c685463b8507883820a1197e1bae5281010d5
Himanshu Jain <himanshujain.adt@gmail.com>
2025-01-21 12:20:41 +0530

    img logo

--- src/NavBar/Navbar.jsx
+++ src/NavBar/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
-        <a className="navbar-brand" href="#">purelyprep</a>
-     
+        {/* <a className="navbar-brand" href="#">purelyprep</a> */}
+     <img src="src\assets\logo.png" alt="" />
         
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
